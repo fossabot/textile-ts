ef5feffcc25ff10ce3cfc6f77acb94cdc26cb1e8
phothinmg <phothinmg@disroot.org>
2025-07-21 02:10:51 +0630

    modified:   src/index.ts

---
+++ a.ts
@@ -0,0 +1,13 @@
+import fs from "node:fs";
+import textile2jml from "./src/lib/jsonml/index.js";
+import { preProcessedString } from "./src/lib/pre-process/index.js";
+import { ml2hast, hast2html } from "./src/lib/hast/hast.js";
+//
+const tx = fs.readFileSync("a.textile", "utf8");
+const txt = preProcessedString(tx);
+const jsonMl = textile2jml(txt);
+const hast = ml2hast(jsonMl);
+const html = hast2html(hast);
+
+//fs.writeFileSync("aa.json", JSON.stringify(hast, null, 2));
+fs.writeFileSync("aa.html", html);
--- src/lib/pre-process/blockCode.ts
+++ src/lib/pre-process/blockCode.ts
@@ -13,89 +13,89 @@
  * ```
  *
  */
-export const stateABlockCode = (input: string) => {
-	const lines = input.split(/\r?\n/);
-	const bcOpenRe = /^bc([^.]*)(\.\.)/;
-	const bcCloseRe = /^..bc$/; // $ for make sure the line has only `..bc`
+export const blockCode = (input: string) => {
+  const lines = input.split(/\r?\n/);
+  const bcOpenRe = /^bc([^.]*)(\.\.)/;
+  const bcCloseRe = /^..bc$/; // $ for make sure the line has only `..bc`
 
-	let cbContent = false;
-	let cbIndex = 0;
-	let line = "";
-	let lineIndex: number | null = null;
-	const newLines: string[] = [];
-	const temp: { id: number; line: string }[] = [];
-	const tempLines: { id: number; line: string }[] = [];
-	const oriLines: { id: number; line: string }[] = [];
-	const preStart = (text: string, id: number) => {
-		const s = bcOpenRe.test(text);
-		const e = bcCloseRe.test(text);
+  let cbContent = false;
+  let cbIndex = 0;
+  let line = "";
+  let lineIndex: number | null = null;
+  const newLines: string[] = [];
+  const temp: { id: number; line: string }[] = [];
+  const tempLines: { id: number; line: string }[] = [];
+  const oriLines: { id: number; line: string }[] = [];
+  const preStart = (text: string, id: number) => {
+    const s = bcOpenRe.test(text);
+    const e = bcCloseRe.test(text);
 
-		if (s && !e) {
-			cbIndex++;
-			cbContent = true;
-			lineIndex = id;
-		}
-	};
-	const preEnd = (text: string) => {
-		const _s = bcOpenRe.test(text);
-		const e = bcCloseRe.test(text);
+    if (s && !e) {
+      cbIndex++;
+      cbContent = true;
+      lineIndex = id;
+    }
+  };
+  const preEnd = (text: string) => {
+    const _s = bcOpenRe.test(text);
+    const e = bcCloseRe.test(text);
 
-		if (cbIndex && e) {
-			cbIndex--;
-			cbContent = false;
-			lineIndex = null;
-		}
-	};
+    if (cbIndex && e) {
+      cbIndex--;
+      cbContent = false;
+      lineIndex = null;
+    }
+  };
 
-	for (let i = 0; i < lines.length; i++) {
-		line = lines[i];
-		preStart(line, i);
-		if (!cbIndex) {
-			newLines.push(line);
-		} else {
-			preEnd(line);
-			if (cbContent) {
-				if (lineIndex) {
-					temp.push({ id: lineIndex, line });
-				}
-				newLines.push("%bc%");
-				tempLines.push({ id: i, line });
-			} else {
-				newLines.push(line);
-			}
-			cbContent = true;
-		}
-	}
+  for (let i = 0; i < lines.length; i++) {
+    line = lines[i];
+    preStart(line, i);
+    if (!cbIndex) {
+      newLines.push(line);
+    } else {
+      preEnd(line);
+      if (cbContent) {
+        if (lineIndex) {
+          temp.push({ id: lineIndex, line });
+        }
+        newLines.push("%bc%");
+        tempLines.push({ id: i, line });
+      } else {
+        newLines.push(line);
+      }
+      cbContent = true;
+    }
+  }
 
-	const group = temp.reduce<Record<number, { id: number; line: string }[]>>(
-		(acc, current) => {
-			if (!acc[current.id]) {
-				acc[current.id] = [];
-			}
-			acc[current.id].push(current);
-			return acc;
-		},
-		{},
-	);
+  const group = temp.reduce<Record<number, { id: number; line: string }[]>>(
+    (acc, current) => {
+      if (!acc[current.id]) {
+        acc[current.id] = [];
+      }
+      acc[current.id].push(current);
+      return acc;
+    },
+    {}
+  );
 
-	const result = Object.keys(group).map((v) => {
-		const arr: { id: number; line: string }[] = group[Number(v)];
-		return arr.reduce(
-			(pre, cur) => ({
-				id: pre.id,
-				line: pre.line + cur.line,
-			}),
-			{ id: arr[0].id, line: "" },
-		);
-	});
+  const result = Object.keys(group).map((v) => {
+    const arr: { id: number; line: string }[] = group[Number(v)];
+    return arr.reduce(
+      (pre, cur) => ({
+        id: pre.id,
+        line: pre.line + "\n" + cur.line,
+      }),
+      { id: arr[0].id, line: "" }
+    );
+  });
 
-	result.forEach((r) => {
-		newLines.splice(r.id, 1, r.line);
-		oriLines.push({ id: r.id, line: lines[r.id] });
-	});
-	return {
-		newLines,
-		tempLines,
-		oriLines,
-	};
+  result.forEach((r) => {
+    newLines.splice(r.id, 1, r.line);
+    oriLines.push({ id: r.id, line: lines[r.id] });
+  });
+  return {
+    newLines,
+    tempLines,
+    oriLines,
+  };
 };
